f5971583b6dee933619e01722a54e0d3ab3fc974
ChadCYB <Chad.CYB@gmail.com>
2015-09-23 00:40:33 +0800

    0916 update
    learning javafx

---
+++ 20150916_hw01/src/application/Main.java
@@ -0,0 +1,40 @@
+package application;
+/*	Author: ³¯«³§e
+ * 	Student ID: 103021076
+ * 	Date: 2015/09/16
+ * 	Uses: JavaFX µøµ¡½m²ß
+ *  Java JDK: 1.8
+ */
+import java.io.IOException;
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  @Override
+  public void start(Stage primaryStage) throws IOException {
+    
+    // constructing our scene
+    URL url = getClass().getResource("HelloWorldScene.fxml");
+    AnchorPane pane = FXMLLoader.load( url );
+    Scene scene = new Scene( pane );
+    
+    
+    // setting the stage
+    primaryStage.setScene( scene );
+    primaryStage.setTitle( "103021076 ³¯«³§e" );
+    primaryStage.setX(320);
+    primaryStage.setY(240);
+    primaryStage.show();
+    
+  }
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+}
